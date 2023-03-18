1093518030c6ae3bbaa13d8eb0e1d7ce7481a80a
Dzmitry-Svi <dima.svitich@yandex.ru>
2023-03-18 18:30:22 +0300

    Решил 2-ю задачу

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Clear();
+Console.WriteLine("Введите координату x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату x2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату x3: ");
+double x3 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y3: ");
+double y3 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Math.Sqrt(Math.Pow(x1 - y1, 2) + Math.Pow(x2 - y2, 2) + Math.Pow(x3 - y3, 2)));
+
